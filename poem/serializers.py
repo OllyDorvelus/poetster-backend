020fec1b6e84f32d68c23495ba75c4218773bc9d
ollydorvelus <ollydorvelus@gmail.com>
2020-06-06 16:12:59 -0400

    cleaning up serializer for poems and sub resources

--- poem/serializers.py
+++ poem/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 
+from django.utils.timesince import timesince
+
 from poem.models import Poem, Genre, Category
+
+from user.serializers import UserSerializer
 
 class GenreSerializer(serializers.ModelSerializer):
     """Serializer for genre - all methods"""
     class Meta:
         model = Genre
         fields = ('id', 'name', 'disabled', 'created', 'updated')
+        read_only_fields = ('id', 'created', 'updated')
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -14,14 +19,38 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'disabled')
+        read_only_fields = ('id',)
 
+class SimpleGenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('id', 'name')
 
 class PoemSerializer(serializers.ModelSerializer):
     """Serializer for creating a poem"""
+    genre = serializers.SlugRelatedField(queryset=Genre.objects.all(), slug_field='name')
+    user = UserSerializer(read_only=True)
+    categories = serializers.SlugRelatedField(queryset=Category.objects.all(), slug_field='name', many=True)
+    timesince = serializers.SerializerMethodField()
 
     class Meta:
         model = Poem
-        fields = ('id', 'title', 'summary', 'content', 'is_published', 'genre', 'categories')
+        fields = ('id',
+                  'user',
+                  'title',
+                  'summary',
+                  'content',
+                  'is_published',
+                  'genre',
+                  'categories',
+                  'poem_count',
+                  'timesince',
+                  'created')
+
+        read_only_fields = ('created', )
+
+    def get_timesince(self, obj):
+        return f'{timesince(obj.created)} ago'
 
 
 class PoemImageSerializer(serializers.ModelSerializer):
@@ -31,5 +60,4 @@ class PoemImageSerializer(serializers.ModelSerializer):
         model = Poem
         fields = ('id', 'image')
         read_only_fields = ('id',)
-
 
