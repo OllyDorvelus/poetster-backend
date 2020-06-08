5ee2f69aa058d3aff3a6738497fa936e85ee55a5
ollydorvelus <ollydorvelus@gmail.com>
2020-06-07 21:27:47 -0400

    modifying user and profile serializers

--- user/serializers.py
+++ user/serializers.py
@@ -1,10 +1,43 @@
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import ugettext_lazy as _
 
+from user.models import Profile
+from poem.models import Poem
+
 from rest_framework import serializers
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    """Serializer for profile model"""
+    poem_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Profile
+        fields = ('subscribers', 'bio', 'instagram', 'poem_count', 'total_subscribers', 'total_subscribed')
+        read_only_fields = ('subscribers',)
+
+    def get_poem_count(self, obj):
+        return Poem.objects.filter(user=obj.user).count()
+
+
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for user model"""
+    profile = ProfileSerializer()
+
+    class Meta:
+        model = get_user_model()
+        fields = ('pen_name', 'first_name', 'last_name', 'profile')
+
+    def update(self, instance, validated_data):
+        instance.pen_name = validated_data['pen_name']
+        instance.first_name = validated_data['first_name']
+        instance.last_name = validated_data['last_name']
+        instance_profile = instance.profile
+        instance_profile.bio = validated_data['profile']['bio']
+        instance_profile.instagram = validated_data['profile']['instagram']
+        return instance
+
+class UserCreateSerializer(serializers.ModelSerializer):
     """Serializer for the users object"""
     password2 = serializers.CharField(min_length=8, trim_whitespace=False, style={'input_type': 'password'}, write_only=True)
     email2 = serializers.EmailField(trim_whitespace=True, style={'input_type': 'email'}, write_only=True)
@@ -45,6 +78,7 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication objects"""
